7b0a78de8b0c9ed8367e00ddb1f069d71dccc44b
Pasquale Pedulla <s4245753@student.rmit.edu.au>
2026-05-25 12:16:00 +1000

    Add files via upload

---
+++ in-class-java-programming-team_111_classwork-main/src/main/java/app/Page4X.java
@@ -0,0 +1,89 @@
+package app;
+
+import java.util.ArrayList;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+/**
+ * Example Index HTML class using Javalin
+ * <p>
+ * Generate a static HTML page using Javalin
+ * by writing the raw HTML into a Java String object
+ *
+ * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
+ * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
+ */
+public class Page4X implements Handler {
+
+    // URL of this page relative to http://localhost:7001/
+    public static final String URL = "4X";
+
+    @Override
+    public void handle(Context context) throws Exception {
+        // Create a simple HTML webpage in a String
+        String html = "<html>";
+
+        // Add some Header information
+        html = html + "<head>" + 
+               "<title>3X Page</title>";
+
+        // Add some CSS (external file)
+        html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
+        html = html + "</head>";
+
+        // Add the body
+        html = html + "<body>";
+
+        // Add the topnav
+        // This uses a Java v15+ Text Block
+        html = html + """
+            <div class='topnav'>
+                <a href='/'>Prog. 2 Sample</a>
+                <a href='2X'>Prog. 2X</a>
+                <a href='2Y'>Prog. 2Y</a>
+                <a href='movies'>Prog. 3 Sample</a>
+                <a href='3X'>Prog. 3X</a>
+                <a href='3Y'>Prog. 3Y</a>
+                <a href='moviestype'>Prog. 4 Sample</a>
+                <a href='4X'>Prog. 4X</a>
+                <a href='4Y'>Prog. 4Y</a>
+            </div>
+        """;
+
+        // Add header content block
+        html = html + """
+            <div class='header'>
+                <h1>
+                    <img src='logo.png' class='top-image' alt='RMIT logo' height='75'>
+                    4X Page
+                </h1>
+            </div>
+        """;
+
+        // Add Div for page Content
+        html = html + """
+            <div class='content'>
+                <p>Use your combined Java, HTML and SQL skills to filter and show data based on the director and movie tables.</p>
+                <p>Use the code for the "Prog. 4 Sample" page in the "PageMoviesType.java" file as reference.</p>
+                <p>And don't forget to add your post method call on the App.java file to make the web forms work.</p>
+            </div>
+            """;
+
+        // Footer
+        html = html + """
+            <div class='footer'>
+                <p>COSC2803 - Java Programming Studio</p>
+            </div>
+        """;
+
+        // Finish the HTML webpage
+        html = html + "</body>" + "</html>";
+
+
+        // DO NOT MODIFY THIS
+        // Makes Javalin render the webpage
+        context.html(html);
+    }
+
+}
